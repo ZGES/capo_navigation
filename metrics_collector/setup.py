a525696d6880d565a0cc9ed05efb9f8c13664bf4
Piotr Pasternak <piotrpassternak@gmail.com>
2022-07-26 13:22:38 +0200

    fix another typo

--- metrics_collector/setup.py
+++ metrics_collector/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'metric = metrics_collector.metric_collectors_node:main',
+            'metric = metrics_collector.metrics_collector_node:main',
         ],
     },
 )
